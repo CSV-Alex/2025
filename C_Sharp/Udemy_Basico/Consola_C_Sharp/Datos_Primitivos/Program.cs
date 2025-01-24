3d0d92285aac31bda6ed3079fc5fb3fe6fa8fd83
CSV-Alex <alexcv2512@gmail.com>
2025-01-23 20:51:44 -0500

    Avance Datos Primitivos

--- C_Sharp/Udemy_Basico/Consola_C_Sharp/Datos_Primitivos/Program.cs
+++ C_Sharp/Udemy_Basico/Consola_C_Sharp/Datos_Primitivos/Program.cs
@@ -10,6 +10,21 @@ namespace Datos_Primitivos
     {
         static void Main(string[] args)
         {
+            int edad;
+            string nombre;
+            double sueldo;
+            bool casado;
+
+            edad = 10;
+            nombre = "Victor";
+            sueldo = 100.50;
+            casado = true;
+
+            Console.WriteLine("Edad es: " + edad);
+            Console.WriteLine("Nombre es: " + nombre);
+            Console.WriteLine("Sueldo es: " + sueldo);
+            Console.WriteLine("Estado civil es: " + casado);
+            Console.ReadKey();
         }
     }
 }
