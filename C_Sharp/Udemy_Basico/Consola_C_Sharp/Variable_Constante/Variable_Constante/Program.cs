dd33ddbc5e9b8ac26fd379b07c322771dd2ffdad
CSV-Alex <alexcv2512@gmail.com>
2025-01-24 20:37:03 -0500

    Variable Constante pruebas

---
+++ C_Sharp/Udemy_Basico/Consola_C_Sharp/Variable_Constante/Variable_Constante/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable_Constante
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int edad;
+            const double sueldo = 12000;
+
+            nombre = "Victor";
+            edad = 25; 
+            Console.WriteLine("Sueldo actual: " + sueldo);
+
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Edad: " + edad);
+            Console.ReadKey();
+        }
+    }
+}
