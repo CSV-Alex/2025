acff666141faf8c83d2c1a1636bde0af530764aa
CSV-Alex <alexcv2512@gmail.com>
2025-01-24 20:49:04 -0500

    Variable Dinamica pruebas

---
+++ C_Sharp/Udemy_Basico/Consola_C_Sharp/Variable_Dinamica/Variable_Dinamica/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable_Dinamica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic variableDinamica = 10;
+            Console.WriteLine(variableDinamica);
+            variableDinamica = "Hola Mundo";
+            Console.WriteLine(variableDinamica);
+            variableDinamica = new { Nombre = "Juan", Edad = 20 };
+            Console.WriteLine(variableDinamica.Nombre);
+            Console.WriteLine(variableDinamica.Edad);
+            Console.ReadKey();
+        }
+    }
+}
