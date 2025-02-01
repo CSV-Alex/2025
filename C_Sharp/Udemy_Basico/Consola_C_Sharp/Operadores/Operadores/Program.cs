86ea98111be288141de6eb9e87a58e937edb76f2
CSV-Alex <alexcv2512@gmail.com>
2025-02-01 11:23:55 -0500

    Operacion relacional basica booleana

--- C_Sharp/Udemy_Basico/Consola_C_Sharp/Operadores/Operadores/Program.cs
+++ C_Sharp/Udemy_Basico/Consola_C_Sharp/Operadores/Operadores/Program.cs
@@ -10,10 +10,12 @@ namespace Operadores
     {
         static void Main(string[] args)
         {
-            int num1, num2, Operaciones;
+            int num1, num2;
+            bool Operaciones;
             num1 = 10;
             num2 = 20;
-            Console.WriteLine("num1 es mayor que num2? " + (num1 > num2));
+            Operaciones = (num1 >= num2);
+            Console.WriteLine("num1 es mayor que num2? " + Operaciones);
         }
     }
 }
