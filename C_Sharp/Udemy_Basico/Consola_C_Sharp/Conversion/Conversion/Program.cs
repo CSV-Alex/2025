fbdaa7a4d59b26e6ab6d743e48aa475aedec5680
CSV-Alex <alexcv2512@gmail.com>
2025-02-01 12:31:01 -0500

    Conversion de datos simple

---
+++ C_Sharp/Udemy_Basico/Consola_C_Sharp/Conversion/Conversion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nombre;
+            int Edad;
+            bool Casado;
+            double Sueldo;
+
+            Console.Write("Ingrese su nombre: ");
+            Nombre = Console.ReadLine();
+
+            Console.Write("Ingrese su Edad: ");
+            Edad = int.Parse(Console.ReadLine());
+
+            Console.Write("Eres Casado ?: ");
+            Casado = bool.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese su Sueldo: ");
+            Sueldo = double.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
