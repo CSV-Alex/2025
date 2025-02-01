9748bfb8a474ddc9cfcb0ea5779cc59ed391b364
CSV-Alex <alexcv2512@gmail.com>
2025-02-01 12:57:51 -0500

    Condiciones IF - ELSE basico

---
+++ C_Sharp/Udemy_Basico/Consola_C_Sharp/Condiciones/Condiciones/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condiciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa tu nombre de iure: ");
+            string Nombre = Console.ReadLine();
+
+            Console.Write("Ingresa tu sueldo: ");
+            int Sueldo = int.Parse(Console.ReadLine());
+
+            if (Sueldo <= 1024)
+            {
+                Console.WriteLine("Eres pobre");
+            }
+            else if (Sueldo <= 2048)
+            {
+                Console.WriteLine("Eres clase media");
+            }
+
+            else if (Sueldo <= 4096)
+            {
+                Console.WriteLine("Eres clase alta");
+            }
+            else
+            {
+                Console.WriteLine("Eres un magnate");
+            }
+
+        }
+    }
+}
