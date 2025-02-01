91f8ea151e807de8f84f8da067aef8bed8a7471c
CSV-Alex <alexcv2512@gmail.com>
2025-02-01 13:13:01 -0500

    Estructura Selectiva SWITCH

---
+++ C_Sharp/Udemy_Basico/Consola_C_Sharp/Estructuras/Estructuras/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estructuras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Dia;
+            Console.Write("Ingrese el dia en formato numerico: ");
+            Dia = int.Parse(Console.ReadLine());
+
+            switch(Dia)
+            {
+                case 1:
+                    Console.WriteLine("Lunes");
+                    break;
+                case 2:
+                    Console.WriteLine("Martes");
+                    break;
+                case 3:
+                    Console.WriteLine("Miercoles");
+                    break;
+                case 4:
+                    Console.WriteLine("Jueves");
+                    break;
+                case 5:
+                    Console.WriteLine("Viernes");
+                    break;
+                case 6:
+                    Console.WriteLine("Sabado");
+                    break;
+                case 7:
+                    Console.WriteLine("Domingo");
+                    break;
+                default:
+                    Console.WriteLine("Ingrese un dia valido, porfavor");
+                    break;
+
+            }
+
+        }
+    }
+}
