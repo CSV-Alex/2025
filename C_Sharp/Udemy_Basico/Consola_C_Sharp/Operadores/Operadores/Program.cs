daf44410bb8244687a28a9a6e6e00deebf8e06f0
CSV-Alex <alexcv2512@gmail.com>
2025-02-01 11:45:59 -0500

    Operadores logicos basicos

--- C_Sharp/Udemy_Basico/Consola_C_Sharp/Operadores/Operadores/Program.cs
+++ C_Sharp/Udemy_Basico/Consola_C_Sharp/Operadores/Operadores/Program.cs
@@ -10,12 +10,23 @@ namespace Operadores
     {
         static void Main(string[] args)
         {
+            /* 
             int num1, num2;
             bool Operaciones;
             num1 = 10;
             num2 = 20;
             Operaciones = (num1 >= num2);
             Console.WriteLine("num1 es mayor que num2? " + Operaciones);
+            */
+
+            int a, b, c, d;
+            bool Resultado;
+            a = 10;
+            b = 2;
+            c = 0;
+            d = 205;
+            Resultado = ((a > b) && (c <= d) && (a > c)) || (a == b);
+            Console.WriteLine("(a > b) && (c <= d) && (a > c) || (a == b) es " + Resultado);
         }
     }
 }
