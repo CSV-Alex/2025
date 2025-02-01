4791545d236e67487d6937d3cc484a537035b29e
CSV-Alex <alexcv2512@gmail.com>
2025-02-01 11:21:52 -0500

    Operaciones relacionales basicas

---
+++ C_Sharp/Udemy_Basico/Consola_C_Sharp/Operadores/Operadores/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, Operaciones;
+            num1 = 10;
+            num2 = 20;
+            Console.WriteLine("num1 es mayor que num2? " + (num1 > num2));
+        }
+    }
+}
