b66607f7f6bc23bf66220514ad6fe783fb07cdcb
CSV-Alex <alexcv2512@gmail.com>
2025-02-01 23:40:04 -0500

    Matrices Basico

---
+++ C_Sharp/Udemy_Basico/Consola_C_Sharp/Matrices/Matrices/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] Persona = new string[3, 2];
+            Persona[0, 0] = "AXEL";
+
+            Console.WriteLine("Nombre: " + Persona[0, 0]);
+        }
+    }
+}
