38e7d5007673365bfc87a54d374c5e03f013418d
CSV-Alex <alexcv2512@gmail.com>
2025-02-01 23:00:31 -0500

    Instruccion FOR

---
+++ C_Sharp/Udemy_Basico/Consola_C_Sharp/Intruccion For/Intruccion For/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intruccion_For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Nentero = 0;
+            Console.WriteLine("Ingrese un numero entero para la tabla de suma: ");
+            Nentero = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i + " + " + Nentero + " = " + (i + Nentero));
+            }
+
+        }
+    }
+}
