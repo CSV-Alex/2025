9e62a1abbad875159d0b66356b57d62c8e3de091
CSV-Alex <alexcv2512@gmail.com>
2025-02-04 15:11:09 -0500

    Excepciones Try Catch basico

---
+++ C_Sharp/Udemy_Basico/Consola_C_Sharp/Excepciones TRY CATCH/Excepciones TRY CATCH/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones_TRY_CATCH
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Edad;
+
+            try
+            {
+                Console.WriteLine("Introduce tu edad: ");
+                Edad = int.Parse(Console.ReadLine());
+                if (Edad < 18)
+                {
+                    Console.WriteLine("Eres menor de edad");
+                }
+                else
+                {
+                    Console.WriteLine("Eres mayor de edad");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
