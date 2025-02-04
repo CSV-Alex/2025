f02131abf6e8b1358b0a6ad1f09b78902f07b96c
CSV-Alex <alexcv2512@gmail.com>
2025-02-04 14:01:19 -0500

    Metodos basico

---
+++ C_Sharp/Udemy_Basico/Consola_C_Sharp/Metodos/Metodos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejemplo de métodos en C#");
+
+            Saludar();
+            IngresoDatos();
+        }
+
+        // Metodo ejemplo 1
+        static void Saludar()
+        {
+            Console.WriteLine("Saludos desde el método Saludar");
+        }
+
+        // Metodo ejemplo 2
+        static void IngresoDatos()
+        {
+            string Nombre;
+            Console.Write("Ingresa tu nombre: ");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + Nombre);
+        }
+    }
+}
---
+++ C_Sharp/Udemy_Basico/Consola_C_Sharp/Vectors/Vectors/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Vectors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Nombres = { "AXEL", "CARLOS", "LUIS", "JORGE" };
+            Console.WriteLine("Nombres de los alumnos: ");
+            foreach (string Nombre in Nombres)
+            {
+                Console.WriteLine(Nombre);
+            }           
+            for (int i = 0; i < Nombres.Length; i++)
+            {
+                Console.WriteLine(Nombres[i]);
+            }
+        }
+    }
+}
