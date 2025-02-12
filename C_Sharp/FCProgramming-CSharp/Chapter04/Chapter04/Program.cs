c2acdf240a462a5b2f8bd384fd08a04c9bcccf69
CSV-Alex <alexcv2512@gmail.com>
2025-02-11 23:30:56 -0500

    Chapter04

---
+++ C_Sharp/FCProgramming-CSharp/Chapter04/Chapter04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Nakov.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
