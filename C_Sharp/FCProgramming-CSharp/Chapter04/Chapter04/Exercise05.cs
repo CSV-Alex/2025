d7ca1a3df92db90652a2526b73bd9ae91cef6375
CSV-Alex <alexcv2512@gmail.com>
2025-02-13 14:14:43 -0500

    Exercise05

---
+++ C_Sharp/FCProgramming-CSharp/Chapter04/Chapter04/Exercise05.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter04
+{
+    internal class Exercise05
+    {
+        public void Execute()
+        {
+            Console.Write("Enter two numbers separated by a space: ");
+            string input = Console.ReadLine();
+            
+            string[] inputPars = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] numbers = new int [inputPars.Length];
+            List<int> list = new List<int>();
+
+            for(int i = 0; i < inputPars.Length; i++)
+            {
+                int.TryParse(inputPars[i], out numbers[i]);
+            }
+
+            int start = numbers[0];
+            int end = numbers[1];
+            int fiveBucle = 0;
+
+            while (fiveBucle < end)
+            {
+                if (fiveBucle <= start)
+                {
+                    fiveBucle += 5;
+                }
+
+                if (fiveBucle > start && fiveBucle < end)
+                {
+                    list.Add(fiveBucle);
+                    fiveBucle += 5;
+                }
+            }
+
+            foreach (int number in list)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
--- C_Sharp/FCProgramming-CSharp/Chapter04/Chapter04/Program.cs
+++ C_Sharp/FCProgramming-CSharp/Chapter04/Chapter04/Program.cs
@@ -16,8 +16,12 @@ namespace Chapter04
             //Exercise02 exercise02 = new Exercise02();
             //exercise02.Execute();
 
-            Exercise03 exercise03 = new Exercise03();
-            exercise03.Execute();
+            //Exercise03 exercise03 = new Exercise03();
+            //exercise03.Execute();
+
+            Exercise05 exercise05 = new Exercise05();
+            exercise05.Execute();
+
         }
     }
 }
