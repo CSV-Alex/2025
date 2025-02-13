0c5e4aec7a4766a22885e41ab91199b01130eeb0
CSV-Alex <alexcv2512@gmail.com>
2025-02-12 21:11:22 -0500

    Exercise01

---
+++ C_Sharp/FCProgramming-CSharp/Chapter04/Chapter04/Exercise01.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter04
+{
+    internal class Exercise01
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Enter 3 numbers please: ");
+            Console.Write("Number A: ");
+            int numberA = Int32.Parse(Console.ReadLine());
+            Console.Write("Number B: ");
+            int numberB = Int32.Parse(Console.ReadLine());
+            Console.Write("Number C: ");
+            int numberC = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("The sum of the numbers is: " + (numberA + numberB + numberC));
+        }
+    }
+}
--- C_Sharp/FCProgramming-CSharp/Chapter04/Chapter04/Program.cs
+++ C_Sharp/FCProgramming-CSharp/Chapter04/Chapter04/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Nakov.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +10,8 @@ namespace Chapter04
     {
         static void Main(string[] args)
         {
+            Exercise01 exercise01 = new Exercise01();
+            exercise01.Execute();
         }
     }
 }
