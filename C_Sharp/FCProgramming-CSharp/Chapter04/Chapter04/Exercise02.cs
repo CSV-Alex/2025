2fce1994a6ca90f67dc4f9d1e93754b80107d99d
CSV-Alex <alexcv2512@gmail.com>
2025-02-13 12:15:43 -0500

    Exercise02

---
+++ C_Sharp/FCProgramming-CSharp/Chapter04/Chapter04/Exercise02.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter04
+{
+    internal class Exercise02
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Enter the radius r of a circle: ");
+            double radius = double.Parse(Console.ReadLine());
+            double area = Math.PI * Math.Pow(radius, 2);
+            double perimeter = 2 * Math.PI * radius;
+            Console.WriteLine("The area of the circle is: " + area);
+            Console.WriteLine("The perimeter of the circle is: " + perimeter);
+        }
+    }
+}
