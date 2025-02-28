286b82b3927ae8a87305c3303fbcdd573d4398ec
CSV-Alex <alexcv2512@gmail.com>
2025-02-27 23:43:43 -0500

    Exercise 01

--- C_Sharp/FCProgramming-CSharp/Chapter05/Chapter05/Exercise01.cs
+++ C_Sharp/FCProgramming-CSharp/Chapter05/Chapter05/Exercise01.cs
@@ -8,10 +8,24 @@ namespace Chapter05
 {
     class Exercise01
     {
-        public void Execute() 
+        public void Execute()
         {
-            Console.WriteLine("Enter a number: ");
-    
+            // Write an if-statement that takes two integer variables and exchanges
+            // their values if the first one is greater than the second one.
+
+            int a = 5;
+            int b = 10;
+
+            if (a > b)
+            {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+            else
+            {
+                Console.WriteLine("a is not greater than b");
+            }
         }
     }
 }
