e193c4fd828359a933b1a69549335dda44b60108
CSV-Alex <alexcv2512@gmail.com>
2025-03-03 23:23:22 -0500

    Exercise08

---
+++ C_Sharp/FCProgramming-CSharp/Chapter05/Chapter05/Exercise08.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter05
+{
+    class Exercise08
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Select a option: ");
+            Console.WriteLine("0. int");
+            Console.WriteLine("1. double");
+            Console.WriteLine("2. string");
+
+            Console.WriteLine("-------------------------------------------");
+
+            int option = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("-------------------------------------------");
+
+            switch (option)
+            {
+                case 0:
+                    Console.WriteLine("Enter a number: ");
+                    int intNumber = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"The number is: {intNumber} + 1: {++intNumber}");
+                    break;
+                case 1:
+                    Console.WriteLine("Enter a number: ");
+                    double doubleNumber = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"The number is: {doubleNumber.ToString("F1")} + 1: {(doubleNumber + 1).ToString("F1")}");
+                    break;
+                case 2:
+                    Console.WriteLine("Enter a string: ");
+                    string stringText = Console.ReadLine();
+                    Console.WriteLine($"The text is: {stringText}*");
+                    break;
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+
+        }
+    }
+}
