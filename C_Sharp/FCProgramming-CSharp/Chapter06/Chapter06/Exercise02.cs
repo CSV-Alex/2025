f83bd51231334bbf02d4d88b4901ff7840aef786
CSV-Alex <alexcv2512@gmail.com>
2025-03-08 23:52:03 -0500

    Exercise02

---
+++ C_Sharp/FCProgramming-CSharp/Chapter06/Chapter06/Exercise02.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter06
+{
+    class Exercise02
+    {
+        public void Execute()
+        {
+            //Not divisible for 3 and 7 simutaneously
+            Console.WriteLine("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % (3*7) != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
--- C_Sharp/FCProgramming-CSharp/Chapter06/Chapter06/Program.cs
+++ C_Sharp/FCProgramming-CSharp/Chapter06/Chapter06/Program.cs
@@ -10,8 +10,10 @@ namespace Chapter06
     {
         static void Main(string[] args)
         {
-            Exercise01 exercise01 = new Exercise01();
-            exercise01.Execute();
+            //Exercise01 exercise01 = new Exercise01();
+            //exercise01.Execute();
+            Exercise02 exercise02 = new Exercise02();
+            exercise02.Execute();
         }
     }
 }
