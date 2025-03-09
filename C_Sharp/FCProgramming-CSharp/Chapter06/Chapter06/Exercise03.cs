de9e8a2c3d7d74976ce94b61b393fc533f289030
CSV-Alex <alexcv2512@gmail.com>
2025-03-08 23:55:15 -0500

    Exercise03

---
+++ C_Sharp/FCProgramming-CSharp/Chapter06/Chapter06/Exercise03.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter06
+{
+    class Exercise03
+    {
+        public void Execute() 
+        {
+            Console.WriteLine("Enter a series of numbers");
+            int[] numbers = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int smallest = numbers[0];
+            int largest = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < smallest)
+                {
+                    smallest = numbers[i];
+                }
+                if (numbers[i] > largest)
+                {
+                    largest = numbers[i];
+                }
+            }
+
+            Console.WriteLine("The smallest number is: " + smallest);
+            Console.WriteLine("The largest number is: " + largest);
+        }
+    }
+}
