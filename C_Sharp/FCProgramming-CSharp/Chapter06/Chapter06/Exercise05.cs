8e23d261e3035b5398a62f9bf805be8ce4f712ef
CSV-Alex <alexcv2512@gmail.com>
2025-03-09 22:34:43 -0500

    Exercise05

---
+++ C_Sharp/FCProgramming-CSharp/Chapter06/Chapter06/Exercise05.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter06
+{
+    class Exercise05
+    {
+        public void Execute()
+        {
+            Console.Write("Enter the N number for Fibonacci: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("---------------------------");
+            int start = 0;
+            int next = 1;
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(start);
+                int temp = start;
+                start = next;
+                next = temp + next;
+            }
+        }
+    }
+}
