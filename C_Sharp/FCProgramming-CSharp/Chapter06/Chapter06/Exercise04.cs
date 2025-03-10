45d752ad6d92698aa372ffa1daefadc024f04e98
CSV-Alex <alexcv2512@gmail.com>
2025-03-09 22:30:30 -0500

    Exercise04

---
+++ C_Sharp/FCProgramming-CSharp/Chapter06/Chapter06/Exercise04.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter06
+{
+    class Exercise04
+    {
+        public void Execute()
+        {
+            Console.WriteLine("All posible cards from a standart deck of 52 cards:");
+            string[] suits = { "Diamonts", "Clubs", "Hearts", "Spades" };
+            string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                               "Jack", "Queen", "King", "Ace" };
+
+            for (int i = 0; i < ranks.Length; i++)
+            {
+                for (int j = 0; j < suits.Length; j++)
+                {
+                    Console.WriteLine(ranks[i] + " of " + suits[j]);
+                }
+            }
+
+        }
+    }
+}
