d1441966dad86f7aed80df015a63d6d65ec52248
CSV-Alex <alexcv2512@gmail.com>
2025-03-10 23:21:02 -0500

    Exercise11

---
+++ C_Sharp/FCProgramming-CSharp/Chapter06/Chapter06/Exercise11.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter06
+{
+    class Exercise11
+    {
+        public void Execute() 
+        { 
+            Console.WriteLine("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            int count = 0;
+            int divisor = 5;
+            long factorial = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+
+            do
+            {
+                count += number / divisor;
+                divisor *= 5;
+            } 
+            while (number / divisor > 0);
+
+            Console.WriteLine("Factorial: " + factorial);
+            Console.WriteLine("Trailing zeros: " + count);
+        }
+    }
+}
