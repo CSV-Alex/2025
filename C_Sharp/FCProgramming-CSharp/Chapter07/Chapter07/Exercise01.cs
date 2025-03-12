92dcea4c66436048eef162123b0ee8f3c0f83d9f
CSV-Alex <alexcv2512@gmail.com>
2025-03-11 22:28:07 -0500

    Exercise01

--- C_Sharp/FCProgramming-CSharp/Chapter07/Chapter07/Exercise01.cs
+++ C_Sharp/FCProgramming-CSharp/Chapter07/Chapter07/Exercise01.cs
@@ -10,7 +10,11 @@ namespace Chapter07
     {
         public void Execute()
         {
-
+            int[] elements = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            for (int i = 0; i < elements.Length; i++)
+            {
+                Console.WriteLine(elements[i] * 5);
+            }
         }
     }
 }
