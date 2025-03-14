a2031dcd39d6b36b8fda9fed80619d31d3f5bd98
CSV-Alex <alexcv2512@gmail.com>
2025-03-13 23:55:32 -0500

    Exercise04

---
+++ C_Sharp/FCProgramming-CSharp/Chapter07/Chapter07/Exercise04.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter07
+{
+    class Exercise04
+    {
+        public void Execute()
+        {
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Enter numbers separated by spaces: ");
+            string input = Console.ReadLine();
+
+            string[] inputNumbers = input.Split(' ');
+
+            foreach (string number in inputNumbers)
+            {
+                if (int.TryParse(number, out int parsedNum))
+                {
+                    numbers.Add(parsedNum);
+                }
+            }
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("No numbers entered.");
+                return;
+            }
+
+            int current = numbers[0];
+            int numberMayor = numbers[0];
+            int count = 1;
+            int maxCount = 1;
+
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (current == numbers[i])
+                {
+                    count++;
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                        numberMayor = current;
+                    }
+                }
+                else
+                {
+                    current = numbers[i];
+                    count = 1;
+                }
+            }
+
+            Console.WriteLine($"The longest sequence of equal numbers has {maxCount} elements.");
+            Console.WriteLine($"The number that appears in the longest sequence is: {numberMayor}");
+        }
+    }
+}
