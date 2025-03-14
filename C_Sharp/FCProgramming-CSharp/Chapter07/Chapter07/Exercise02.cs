ac586181688de597ea066daa7afd706eb819e8f1
CSV-Alex <alexcv2512@gmail.com>
2025-03-13 21:34:32 -0500

    Exercise02

--- C_Sharp/FCProgramming-CSharp/Chapter07/Chapter07/Exercise02.cs
+++ C_Sharp/FCProgramming-CSharp/Chapter07/Chapter07/Exercise02.cs
@@ -8,5 +8,43 @@ namespace Chapter07
 {
     class Exercise02
     {
+        public void Execute()
+        {
+            Console.WriteLine("Enter names separated by space:");
+            string[] names = Console.ReadLine().Split(' ');
+            Console.WriteLine("Enter names to compare separated by space:");
+            string[] compareNames = Console.ReadLine().Split(' ');
+
+            if (names.Length != compareNames.Length)
+            {
+                Console.WriteLine("Arrays are not equal");
+                return;
+            }
+
+            bool areEqual = true;
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i] != compareNames[i] && areEqual != false)
+                {
+                    Console.WriteLine("Array {0} are not equal in index", names[i]);
+                    areEqual = false;
+                }
+                else
+                {
+                    Console.WriteLine("Arrays {0} are equal on index", compareNames[i]);
+                }
+            }
+
+            if (areEqual)
+            {
+                Console.WriteLine("--------------------------------------------------------");
+                Console.WriteLine("Arrays are equal and have the same index");
+            }
+            else
+            {
+                Console.WriteLine("--------------------------------------------------------");
+                Console.WriteLine("Arrays are NOT equal and have different index");
+            }
+        }
     }
 }
